c953f2e3ef70dfcb79f13a0a24c3dc6e909398de
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-12 22:32:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    self.word = word
+  end
+  
+  def match(array)
+    matches = []
+    array.each do |x|
+      if x.split('').sort == self.word.split('').sort
+        matches << x
+      end
+    end
+    matches
+end
